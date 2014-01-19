544a0bcab9bf560f11f630d1d72684f89b926141
Andrew Langemann <andyjl@me.com>
2014-01-18 20:55:49 -0700

    Built first hebrew-vocab1 quiz

---
+++ js/quizzes/hebrew-vocab1.js
@@ -0,0 +1,155 @@
+QuizIt.addQuiz({
+
+    name: 'hebrew-vocab1',
+    messageText: 'Translate this Hebrew word into English',
+
+    promptField: 'hebrewWord',
+    answerField: 'englishWords',
+
+    problems: [
+        // Unit 1
+        { 
+            hebrewWord: "אני (ani)", 
+            englishWords: ["I"] 
+        },
+        { 
+            hebrewWord: "אתה (ata, m)", 
+            englishWords: ["you"] 
+        },
+        { 
+            hebrewWord: "את (at, f)", 
+            englishWords: ["you"] 
+        },
+        { 
+            hebrewWord: "מי? (mi?)", 
+            englishWords: ["who"] 
+        },
+        { 
+            hebrewWord: "כן (ken)", 
+            englishWords: ["yes"] 
+        },
+        { 
+            hebrewWord: "לא (lo)", 
+            englishWords: ["no"] 
+        },
+        { 
+            hebrewWord: "שלום (shalom)", 
+            englishWords: ["hello", "goodbye"] 
+        },
+        { 
+            hebrewWord: "להתראות (lehitra'ot)", 
+            englishWords: ["see you"] 
+        },
+        { 
+            hebrewWord: "נעים מאוד (na'im me'od)", 
+            englishWords: ["nice to meet you"] 
+        },
+        { 
+            hebrewWord: "ו.. (ve)", 
+            englishWords: ["and"] 
+        },
+        { 
+            hebrewWord: "אבא (aba)", 
+            englishWords: ["father"] 
+        },
+        { 
+            hebrewWord: "דג (dag)", 
+            englishWords: ["fish"] 
+        },
+        { 
+            hebrewWord: "בגד (beged)", 
+            englishWords: ["garment"] 
+        },
+        { 
+            hebrewWord: "חבר (xaver)", 
+            englishWords: ["friend"] 
+        },
+        { 
+            hebrewWord: "חברה (xavera, f)", 
+            englishWords: ["friends"] 
+        },
+        { 
+            hebrewWord: "חברים (xaverim, m)", 
+            englishWords: ["friends"] 
+        },
+        { 
+            hebrewWord: "חברות (xaverot, f)", 
+            englishWords: ["friends"] 
+        },
+
+        // Unit 2
+        { 
+            hebrewWord: "הוא (hu)", 
+            englishWords: ["he"] 
+        },
+        { 
+            hebrewWord: "היא (hi)", 
+            englishWords: ["she"] 
+        },
+        { 
+            hebrewWord: "מורה (more)", 
+            englishWords: ["teacher"] 
+        },
+        { 
+            hebrewWord: "מורה (mora)", 
+            englishWords: ["teacher"] 
+        },
+        { 
+            hebrewWord: "תלמיד (talmid)", 
+            englishWords: ["student"] 
+        },
+        { 
+            hebrewWord: "תלמידה (talmida)", 
+            englishWords: ["student"] 
+        },
+        { 
+            hebrewWord: "ילד (yeled)", 
+            englishWords: ["boy"] 
+        },
+        { 
+            hebrewWord: "ילדה (yalda)", 
+            englishWords: ["girl"] 
+        },
+        { 
+            hebrewWord: "מאיפה (me'eifo?)", 
+            englishWords: ["where from"] 
+        },
+        { 
+            hebrewWord: "או (O)", 
+            englishWords: ["or"] 
+        },
+        { 
+            hebrewWord: "גם (gam)", 
+            englishWords: ["also"] 
+        },
+        { 
+            hebrewWord: "מ... (mi / me)", 
+            englishWords: ["from"] 
+        },
+        { 
+            hebrewWord: "דגל (degel)", 
+            englishWords: ["flag"] 
+        },
+        { 
+            hebrewWord: "גלידה (glida)", 
+            englishWords: ["ice cream"] 
+        },
+        { 
+            hebrewWord: "כלב (kelev)", 
+            englishWords: ["dog"] 
+        },
+        { 
+            hebrewWord: "חג (xag)", 
+            englishWords: ["holiday"] 
+        },
+        { 
+            hebrewWord: "יד (yad)", 
+            englishWords: ["hand"] 
+        },
+        { 
+            hebrewWord: "אוכל (oxel)", 
+            englishWords: ["food"] 
+        }
+    ]
+
+});
