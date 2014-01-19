d5e5bdceddffbee9af670ab9eeee90ad68738db7
Andrew Langemann <andyjl@me.com>
2014-01-18 17:17:51 -0700

    Revert "Revert "Initial commit""
    This reverts commit 342565548754b201a7c56c47a0831f536ab4f19b.

---
+++ js/quiz-it-web.js
@@ -0,0 +1,81 @@
+QuizItWeb = {
+
+    /* Variables */
+    activeQuiz: null,
+
+    /* Helpers */
+    _getParameterByName: function (name) {
+        name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+        var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+            results = regex.exec(location.search);
+        return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+    },
+
+    /* Event Handlers */
+    onAnswerInput_keydown: function (e) {
+        if (e.keyCode === 13) {
+            QuizItWeb.guess();
+        }
+    },
+
+    onGuessButton_click: function () {
+        QuizItWeb.guess();
+    },
+
+    /* Methods */
+    init: function () {
+        var quizName = this._getParameterByName("quiz");
+
+        try {
+            QuizIt.start(quizName);
+        } catch (ex) {
+            alert(ex);
+        }
+
+        this.activeQuiz = QuizIt.getActiveQuiz();
+
+        // Set up some things.
+        $(".js-message").html(QuizIt.getMessageText());
+        this._update();
+
+        $(".js-guess-button").on("click", this.onGuessButton_click);
+        $(".js-answer-input").on("keydown", this.onAnswerInput_keydown);
+    },
+
+    guess: function () {
+        var answer = $(".js-answer-input").val();
+        var right;
+        var message;
+
+        try {
+            right = QuizIt.guess(answer);
+        } catch (ex) {
+            message = ex;
+        }
+
+        if (right === true) {
+            message = "You got it right!";
+            $(".js-answer-input").val(null);
+        } else if (right === false) {
+            message = "You got it wrong.";
+        }
+
+        $(".js-response").html(message);
+
+        this._update();
+
+        $(".js-answer-input").select();
+    },
+
+    _update: function () {
+        $(".js-prompt").html(QuizIt.getCurrentPrompt());
+
+        // Update the progress indicators.
+        var progress = QuizIt.getProgress();
+        $(".js-progress-indicator").html("{0} of {1}".format(
+                progress.rightCount + progress.wrongCount + 1, QuizIt.getProblemTotal()));
+        $(".js-right-count").html("{0} right".format(progress.rightCount));
+        $(".js-wrong-count").html("{0} wrong".format(progress.wrongCount));
+    }
+
+};
---
+++ js/quizzes/hebrew-alphabet.js
@@ -0,0 +1,39 @@
+QuizIt.addQuiz({
+
+    name: 'hebrew-alphabet',
+    messageText: 'What is the name of this letter?',
+
+    promptField: 'letter',
+    answerField: 'name',
+
+    problems: [
+        { letter: 'א', name: 'Aleph' },
+        { letter: 'ב', name: 'Bet' },
+        { letter: 'ג', name: 'Gimel' },
+        { letter: 'ד', name: 'Dalet' }
+        // { letter: 'ה', name: 'Hey' },
+        // { letter: 'ו', name: 'Vav' },
+        // { letter: 'ז', name: 'Zayin' },
+        // { letter: 'ח', name: 'Chet' },
+        // { letter: 'ט', name: 'Tet' },
+        // { letter: 'י', name: 'Yud' },
+        // { letter: 'כ', name: 'Kaph' },
+        // { letter: 'ך', name: 'Final Kaph' },
+        // { letter: 'ל', name: 'Lamed' },
+        // { letter: 'מ', name: 'Mem' },
+        // { letter: 'ם', name: 'Final Mem' },
+        // { letter: 'נ', name: 'Nun' },
+        // { letter: 'ן', name: 'Final Nun' },
+        // { letter: 'ס', name: 'Samech' },
+        // { letter: 'ע', name: 'Ayin' },
+        // { letter: 'פ', name: 'Pey' },
+        // { letter: 'ף', name: 'Final Pey' },
+        // { letter: 'צ', name: 'Tsade' },
+        // { letter: 'ץ', name: 'Final Tsade' },
+        // { letter: 'ק', name: 'Quph' },
+        // { letter: 'ר', name: 'Resh' },
+        // { letter: 'ש', name: 'Shin' },
+        // { letter: 'ת', name: 'Tav' }
+    ]
+
+});
