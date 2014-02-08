009fd75d794d8467d7a55072097dddfe5b884ac4
Andrew Langemann <andyjl@me.com>
2014-02-07 18:43:44 -0700

    Combined the Hebrew vocal quizzes

---
+++ js/quizzes/hebrew-vocab.js
@@ -0,0 +1,536 @@
+var hebrewVocabProblems = [
+    // Unit 1
+    {
+        hebrewWord: "אני",
+        transliteration: "ani",
+        englishWords: ["I"]
+    },
+    {
+        hebrewWord: "אתה",
+        transliteration: "ata, m",
+        englishWords: ["you"]
+    },
+    {
+        hebrewWord: "את",
+        transliteration: "at, f",
+        englishWords: ["you"]
+    },
+    {
+        hebrewWord: "מי?",
+        transliteration: "mi?",
+        englishWords: ["who"]
+    },
+    {
+        hebrewWord: "כן",
+        transliteration: "ken",
+        englishWords: ["yes"]
+    },
+    {
+        hebrewWord: "לא",
+        transliteration: "lo",
+        englishWords: ["no"]
+    },
+    {
+        hebrewWord: "שלום",
+        transliteration: "shalom",
+        englishWords: ["hello", "goodbye"]
+    },
+    {
+        hebrewWord: "להתראות",
+        transliteration: "lehitra'ot",
+        englishWords: ["see you", "goodbye", "bye bye"]
+    },
+    {
+        hebrewWord: "נעים מאוד",
+        transliteration: "na'im me'od",
+        englishWords: ["nice to meet you"]
+    },
+    {
+        hebrewWord: "ו..",
+        transliteration: "ve",
+        englishWords: ["and"]
+    },
+    {
+        hebrewWord: "אבא",
+        transliteration: "aba",
+        englishWords: ["father"]
+    },
+    {
+        hebrewWord: "דג",
+        transliteration: "dag",
+        englishWords: ["fish"]
+    },
+    {
+        hebrewWord: "בגד",
+        transliteration: "beged",
+        englishWords: ["garment"]
+    },
+    {
+        hebrewWord: "חבר",
+        transliteration: "xaver",
+        englishWords: ["friend"]
+    },
+    {
+        hebrewWord: "חברה",
+        transliteration: "xavera, f",
+        englishWords: ["friend"]
+    },
+    {
+        hebrewWord: "חברים",
+        transliteration: "xaverim, m",
+        englishWords: ["friends"]
+    },
+    {
+        hebrewWord: "חברות",
+        transliteration: "xaverot, f",
+        englishWords: ["friends"]
+    },
+
+    // Unit 2
+    {
+        hebrewWord: "הוא",
+        transliteration: "hu",
+        englishWords: ["he"]
+    },
+    {
+        hebrewWord: "היא",
+        transliteration: "hi",
+        englishWords: ["she"]
+    },
+    {
+        hebrewWord: "מורה",
+        transliteration: "more",
+        englishWords: ["teacher"]
+    },
+    {
+        hebrewWord: "מורה",
+        transliteration: "mora",
+        englishWords: ["teacher"]
+    },
+    {
+        hebrewWord: "תלמיד",
+        transliteration: "talmid",
+        englishWords: ["student"]
+    },
+    {
+        hebrewWord: "תלמידה",
+        transliteration: "talmida",
+        englishWords: ["student"]
+    },
+    {
+        hebrewWord: "ילד",
+        transliteration: "yeled",
+        englishWords: ["boy"]
+    },
+    {
+        hebrewWord: "ילדה",
+        transliteration: "yalda",
+        englishWords: ["girl"]
+    },
+    {
+        hebrewWord: "מאיפה",
+        transliteration: "me'eifo?",
+        englishWords: ["where from"]
+    },
+    {
+        hebrewWord: "או",
+        transliteration: "o",
+        englishWords: ["or"]
+    },
+    {
+        hebrewWord: "גם",
+        transliteration: "gam",
+        englishWords: ["also"]
+    },
+    {
+        hebrewWord: "מ...",
+        transliteration: "mi / me",
+        englishWords: ["from"]
+    },
+    {
+        hebrewWord: "דגל",
+        transliteration: "degel",
+        englishWords: ["flag"]
+    },
+    {
+        hebrewWord: "גלידה",
+        transliteration: "glida",
+        englishWords: ["ice cream"]
+    },
+    {
+        hebrewWord: "כלב",
+        transliteration: "kelev",
+        englishWords: ["dog"]
+    },
+    {
+        hebrewWord: "חג",
+        transliteration: "xag",
+        englishWords: ["holiday"]
+    },
+    {
+        hebrewWord: "יד",
+        transliteration: "yad",
+        englishWords: ["hand"]
+    },
+    {
+        hebrewWord: "אוכל",
+        transliteration: "oxel",
+        englishWords: ["food"]
+    },
+    {
+        hebrewWord: "איש",
+        transliteration: "ish",
+        englishWords: "man"
+    },
+    {
+        hebrewWord: "אישה",
+        transliteration: "isha",
+        englishWords: "woman"
+    },
+
+    // Unit 3
+    {
+        hebrewWord: "גר",
+        transliteration: "gar, m",
+        englishWords: ["live"]
+    },
+    {
+        hebrewWord: "גרה",
+        transliteration: "gara, f",
+        englishWords: ["live"]
+    },
+    {
+        hebrewWord: "לומד",
+        transliteration: "lomed, m",
+        englishWords: ["learn"]
+    },
+    {
+        hebrewWord: "לומדת",
+        transliteration: "lomedet, f",
+        englishWords: ["learn"]
+    },
+    {
+        hebrewWord: "מה?",
+        transliteration: "ma?",
+        englishWords: ["what"]
+    },
+    {
+        hebrewWord: "מה נשמע?",
+        transliteration: "ma nishma?",
+        englishWords: ["How are you", "whats up", "what's up"]
+    },
+    {
+        hebrewWord: "איפה?",
+        transliteration: "eifo?",
+        englishWords: ["where"]
+    },
+    {
+        hebrewWord: "אוטובוס",
+        transliteration: "otobus",
+        englishWords: ["bus"]
+    },
+    {
+        hebrewWord: "אימא",
+        transliteration: "ima",
+        englishWords: ["mother"]
+    },
+    {
+        hebrewWord: "יין",
+        transliteration: "yayin",
+        englishWords: ["wine"]
+    },
+    {
+        hebrewWord: "ים",
+        transliteration: "yam",
+        englishWords: ["sea"]
+    },
+    {
+        hebrewWord: "כסף",
+        transliteration: "kesef",
+        englishWords: ["money"]
+    },
+    {
+        hebrewWord: "מים",
+        transliteration: "mayim",
+        englishWords: ["water"]
+    },
+    {
+        hebrewWord: "מלך",
+        transliteration: "melex",
+        englishWords: ["king"]
+    },
+    {
+        hebrewWord: "מלכה",
+        transliteration: "malka",
+        englishWords: ["queen"]
+    },
+    {
+        hebrewWord: "עוגה",
+        transliteration: "uga",
+        englishWords: ["cake"]
+    },
+    {
+        hebrewWord: "עט",
+        transliteration: "et",
+        englishWords: ["pen"]
+    },
+    {
+        hebrewWord: "עין",
+        transliteration: "ayin",
+        englishWords: ["eye"]
+    },
+    {
+        hebrewWord: "פה",
+        transliteration: "pe",
+        englishWords: ["mouth"]
+    },
+    {
+        hebrewWord: "מצוין",
+        transliteration: "metsuyan",
+        englishWords: ["great"]
+    },
+    {
+        hebrewWord: "טוב",
+        transliteration: "tov",
+        englishWords: ["good"]
+    },
+    {
+        hebrewWord: "טוב מאוד",
+        transliteration: "tov me'od",
+        englishWords: ["very good"]
+    },
+    {
+        hebrewWord: "בסדר",
+        transliteration: "beseder",
+        englishWords: ["OK"]
+    },
+    {
+        hebrewWord: "ככה ככה",
+        transliteration: "kaxa kaxa",
+        englishWords: ["so so"]
+    },
+    {
+        hebrewWord: "רע",
+        transliteration: "ra",
+        englishWords: ["bad"]
+    },
+    {
+        hebrewWord: "רע מאוד",
+        transliteration: "ra me'od",
+        englishWords: ["very bad"]
+    },
+
+    // Unit 4
+    {
+        hebrewWord: "אנשים",
+        transliteration: "anashim",
+        englishWords: ["men", "people"]
+    },
+    {
+        hebrewWord: "נשים",
+        transliteration: "isha",
+        englishWords: ["women"]
+    },
+    {
+        hebrewWord: "ארץ",
+        transliteration: "erets",
+        englishWords: ["country", "land"]
+    },
+    {
+        hebrewWord: "ארצות",
+        transliteration: "aratsot",
+        englishWords: ["countries", "lands"]
+    },
+    {
+        hebrewWord: "בית",
+        transliteration: "bayit",
+        englishWords: ["house"]
+    },
+    {
+        hebrewWord: "בתים",
+        transliteration: "batim",
+        englishWords: ["houses"]
+    },
+    {
+        hebrewWord: "מחברת",
+        transliteration: "maxberet",
+        englishWords: ["notebook"]
+    },
+    {
+        hebrewWord: "מחברות",
+        transliteration: "maxbarot",
+        englishWords: ["notebooks"]
+    },
+    {
+        hebrewWord: "מחשב",
+        transliteration: "maxshev",
+        englishWords: ["computer"]
+    },
+    {
+        hebrewWord: "מחשבים",
+        transliteration: "maxshevim",
+        englishWords: ["computers"]
+    },
+    {
+        hebrewWord: "ספר",
+        transliteration: "sefer",
+        englishWords: ["book"]
+    },
+    {
+        hebrewWord: "ספרים",
+        transliteration: "sfarim",
+        englishWords: ["books"]
+    },
+    {
+        hebrewWord: "עיר",
+        transliteration: "ir",
+        englishWords: ["city", "town"]
+    },
+    {
+        hebrewWord: "ערים",
+        transliteration: "arim",
+        englishWords: ["cities", "towns"]
+    },
+    {
+        hebrewWord: "שעון",
+        transliteration: "sha'on",
+        englishWords: ["clock", "watch"]
+    },
+    {
+        hebrewWord: "שעונים",
+        transliteration: "she'onim",
+        englishWords: ["clocks", "watches"]
+    },
+    {
+        hebrewWord: "שקל",
+        transliteration: "shekel",
+        englishWords: ["shekel"]
+    },
+    {
+        hebrewWord: "שקלים",
+        transliteration: "shkalim",
+        englishWords: ["shekels"]
+    },
+    {
+        hebrewWord: "שמש",
+        transliteration: "shemesh",
+        englishWords: ["sun"]
+    },
+    {
+        hebrewWord: "זה",
+        transliteration: "ze, m",
+        englishWords: ["this"]
+    },
+    {
+        hebrewWord: "זאת",
+        transliteration: "zot, f",
+        englishWords: ["this"]
+    },
+    {
+        hebrewWord: "אלה",
+        transliteration: "ele",
+        englishWords: ["these"]
+    },
+    {
+        hebrewWord: "של",
+        transliteration: "shel",
+        englishWords: ["of"]
+    },
+    {
+        hebrewWord: "ה",
+        transliteration: "ha",
+        englishWords: ["the"]
+    },
+    {
+        hebrewWord: "זכר",
+        transliteration: "zaxar",
+        englishWords: ["masculine"]
+    },
+    {
+        hebrewWord: "נקבה",
+        transliteration: "nekeva",
+        englishWords: ["feminine"]
+    },
+    {
+        hebrewWord: "יחיד",
+        transliteration: "yaxid",
+        englishWords: ["singular"]
+    },
+    {
+        hebrewWord: "רבים",
+        transliteration: "rabim",
+        englishWords: ["plural"]
+    },
+
+    // Extras
+    {
+        hebrewWord: "בטוח",
+        transliteration: "betax",
+        englishWords: ["sure"]
+    },
+    {
+        hebrewWord: "מה שלומך",
+        transliteration: "ma shlomxa, m",
+        englishWords: ["how are you"]
+    },
+    {
+        hebrewWord: "מה שלומך",
+        transliteration: "ma shlomex, f",
+        englishWords: ["how are you"]
+    },
+    {
+        hebrewWord: "תודה",
+        transliteration: "toda",
+        englishWords: ["thank you", "thanks"]
+    },
+    {
+        hebrewWord: "שם",
+        transliteration: "sham",
+        englishWords: ["there"]
+    },
+    {
+        hebrewWord: "עכשיו",
+        transliteration: "axshav",
+        englishWords: ["now"]
+    },
+    {
+        hebrewWord: "עברית",
+        transliteration: "ivrit",
+        englishWords: ["Hebrew"]
+    }
+];
+
+QuizIt.addQuiz({
+
+    name: 'hebrew-vocab-hardcore',
+    messageText: 'Translate this Hebrew word into English',
+
+    promptField: 'hebrewWord',
+    transliteration: "",
+    answerField: 'englishWords',
+
+    problems: hebrewVocabProblems
+
+});
+
+var vocabQuiz = {
+    name: 'hebrew-vocab1',
+    messageText: 'Translate this Hebrew word into English',
+
+    promptField: 'hebrewWord',
+    transliteration: "",
+    answerField: 'englishWords',
+
+    problems: []
+};
+
+// Set the problems - include transliteration.
+$.each(hebrewVocabProblems, function (index, problem) {
+    vocabQuiz.problems.push({
+        hebrewWord: "{0} ({1})".format(problem.hebrewWord, problem.transliteration),
+        transliteration: "",
+        englishWords: problem.englishWords
+    });
+});
+
+QuizIt.addQuiz(vocabQuiz);
